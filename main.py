6d1bf4dbfc5c9e85cdef350836bb1487796b9036
Miguelristo12 <midisgames@gmail.com>
2023-12-11 16:12:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import gym
+env = gym.make("Blackjack-v1")
+observation, info = env.reset(seed=42)
+
+for _ in range(1000):
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+env.close()
