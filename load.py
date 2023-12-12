477df06c84c1b22eb47e69c9e4418013ed4bc58e
Rafaparkoureiro <rafaelpc02@gmail.com>
2023-12-12 18:33:56 +0000

    CHANGES

--- load.py
+++ load.py
@@ -1,7 +1,24 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
-from cona import TupleToMultiDiscreteWrapper
+import numpy as np
 
+class TupleToMultiDiscreteWrapper(gym.ObservationWrapper):
+    def __init__(self, env):
+        super(TupleToMultiDiscreteWrapper, self).__init__(env)
+        parametro_1 = self.observation_space.spaces[0].n
+        parametro_2 = self.observation_space.spaces[1].n
+        parametro_3 = self.observation_space.spaces[2].n
+
+        self.observation_space = gym.spaces.MultiDiscrete([parametro_1,parametro_2,parametro_3])
+
+    def observation(self, observation):
+        parametro_1 = observation[0]
+        parametro_2 = observation[1]
+        parametro_3 = observation[2]
+        observation_space = np.array([parametro_1,parametro_2,parametro_3])
+        return observation_space
+        # Convert Tuple observation to MultiDiscrete observation
+        
 
 print ("cona pesada")
 models_dir = "models/Blackjack/PPO"
@@ -20,5 +37,8 @@ for ep in range(episodes):
     done = False
     while not done:
         action, _states = model.predict(obs)
-        obs, rewards, trunc, done, info = env.step(action)
+        obs, rewards, done, trunc, info = env.step(action)
+        print("obs: ", obs)
+        print("action", action)
+        print("reward", rewards)
         env.render()
