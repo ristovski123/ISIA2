8af0cd5acf7f66c1ffc5762f8e2ce5bfce901aa5
Miguelristo12 <midisgames@gmail.com>
2023-12-15 00:24:59 +0000

    treino dos modelitos com double

--- PPOTunned.py
+++ PPOTunned.py
@@ -3,6 +3,7 @@ from stable_baselines3 import PPO
 import numpy as np
 import os
 
+
 class TupleToMultiDiscreteWrapper(gymnasium.ObservationWrapper):
     def __init__(self, env):
         super(TupleToMultiDiscreteWrapper, self).__init__(env)
@@ -10,16 +11,15 @@ class TupleToMultiDiscreteWrapper(gymnasium.ObservationWrapper):
         parametro_2 = self.observation_space.spaces[1].n
         parametro_3 = self.observation_space.spaces[2].n
 
-        self.observation_space = gymnasium.spaces.MultiDiscrete([parametro_1,parametro_2,parametro_3])
+        self.observation_space = gymnasium.spaces.MultiDiscrete([parametro_1, parametro_2, parametro_3])
 
     def observation(self, observation):
         parametro_1 = observation[0]
         parametro_2 = observation[1]
         parametro_3 = observation[2]
-        observation_space = np.array([parametro_1,parametro_2,parametro_3])
+        observation_space = np.array([parametro_1, parametro_2, parametro_3])
         return observation_space
         # Convert Tuple observation to MultiDiscrete observation
-        
 
 
 models_dir = "models/OriginalBlackjack/PPO"
@@ -32,21 +32,21 @@ if not os.path.exists(logdir):
     os.makedirs(logdir)
 
 # Example usage
-env = gymnasium.make("Blackjack-v1",render_mode = "rgb_array")  # Replace with the actual name of your environment
+env = gymnasium.make("Blackjack-v1", render_mode="rgb_array")  # Replace with the actual name of your environment
 env = TupleToMultiDiscreteWrapper(env)
 
 observation, info = env.reset()
+custom_gamma = 0.95
+custom_learning_rate = 0.001
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
 # model = A2C('MlpPolicy', env, verbose=1)
-#model.learn(total_timesteps=10000)
-
+# model.learn(total_timesteps=10000)
 
 
 TIMESTEPS = 10000
 iters = 0
 for i in range(1500):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
-    model.save(f"{models_dir}/{TIMESTEPS*i}")
-
+    model.save(f"{models_dir}/{TIMESTEPS * i}")
 
 # Now, env has a MultiDiscrete observation space
